0d4a187aafed2d0814a0e165d85613a19b55a3be
j21166675 <johney.mkc@gmail.com>
2020-06-18 11:14:57 -0700

    small corrections for some edge cases to be passed

--- src/Sorting/LomutosPartitionImpl.java
+++ src/Sorting/LomutosPartitionImpl.java
@@ -3,27 +3,27 @@ package Sorting;
 public class LomutosPartitionImpl {
 
     private int[] partition(int[] arr) {
-        int pivot = arr[arr.length-1];
-        int smaller = -1;
-        int i=1;
-        for( i=1;i<=arr.length-1;i++){
-            while(smaller <= 0 && arr[i] < pivot){
-                smaller++;
-                if(i != smaller){
-                    int temp = arr[i];
-                    arr[i] = arr[smaller];
-                    arr[smaller] = temp;
-                }
-            }
-        }
-        arr[i-1] = arr[smaller+1] ;
-        arr[smaller+1] = pivot;
-        return arr;
+      int pivot = arr[arr.length-1];
+      int j = -1;
+      int i=0;
+      for(i=0;i<=arr.length-2;i++){
+          if(pivot >= arr[i]){
+              j++;
+              if(i!=j){
+                  int temp = arr[j];
+                  arr[j] = arr[i];
+                  arr[i] = temp;
+              }
+          }
+      }
+      arr[i] = arr[j+1];
+      arr[j+1] = pivot;
+      return arr;
     }
 
     public static void main(String a[]) throws Exception {
         LomutosPartitionImpl obj = new LomutosPartitionImpl();
-        for (int i : obj.partition(new int[]{11, 3, 10, 2, 8})) {
+        for (int i : obj.partition(new int[]{1,11, 3, 10, 2, 2})) {
             System.out.println(i);
         }
     }
