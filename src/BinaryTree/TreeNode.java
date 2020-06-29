fb15da94722dea63d0822e499d110d9642b20b82
j21166675 <johney.mkc@gmail.com>
2020-06-28 22:08:41 -0700

    Binary tree main class

---
+++ src/BinaryTree/TreeNode.java
@@ -0,0 +1,12 @@
+package BinaryTree;
+
+public class TreeNode{
+    int data;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int data){
+        this.data = data;
+       right = left = null;
+
+    }
+}
