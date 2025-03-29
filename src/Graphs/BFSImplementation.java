910d0327c8e0e6175df133128d929c8f7d04f306
j21166675 <johney.mkc@gmail.com>
2025-03-28 21:48:36 -0700

    Graphs Practice

---
+++ src/Graphs/BFSImplementation.java
@@ -0,0 +1,40 @@
+package Graphs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BFSImplementation {
+    List<List<Integer>> adjacentList;
+    BFSImplementation(){
+        adjacentList = Graphs.adjacentList;
+    }
+    public void bfs(int vertex){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(vertex);
+
+        int size = adjacentList.size();
+        boolean[] visited = new boolean[size];
+        visited[vertex] = true;
+
+        while(!queue.isEmpty()){
+            int curr = queue.poll();
+           for(int neighbor: adjacentList.get(curr)){
+               if(!visited[neighbor]){
+                   queue.add(neighbor);
+                   visited[neighbor] = true;
+               }
+           }
+           System.out.println(curr);
+        }
+    }
+
+    public static void main(String[] a){
+        Graphs.createAdjacentList(5);
+        //Graphs.printNeighbours(2);
+
+        BFSImplementation obj = new BFSImplementation();
+        obj.bfs(0);
+    }
+}
---
+++ src/Graphs/Graphs.java
@@ -0,0 +1,49 @@
+package Graphs;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Graphs {
+    static List<List<Integer>> adjacentList;
+    static int size;
+
+    Graphs(int adjSize){
+        size = adjSize;
+        adjacentList = new ArrayList<>(size);
+
+        for(int i=0; i<size; i++){
+            adjacentList.add(new ArrayList<Integer>());
+        }
+    }
+
+    public static int size(){
+        return size;
+    }
+
+    public static void addEdges(int start, int end, boolean directed){
+        adjacentList.get(start).add(end);
+        if(!directed){
+            adjacentList.get(end).add(start);
+        }
+    }
+
+    public static void printNeighbours(int vertex){
+          for(int i : adjacentList.get(vertex)){
+              System.out.print(i + " ");
+        }
+    }
+
+    public static void createAdjacentList(int size){
+        Graphs g = new Graphs(size);
+        addEdges(0, 1, true);
+        addEdges(0, 2, true);
+        addEdges(1, 0, true);
+        addEdges(1, 2, true);
+        addEdges(2, 0, true);
+        addEdges(2, 1, true);
+        addEdges(2, 3, true);
+        addEdges(2, 4, true);
+        addEdges(3, 2, true);
+        addEdges(4, 2, true);
+    }
+}
